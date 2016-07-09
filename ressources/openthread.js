d52a8b9bd437fe9e24a887fa517b8b142eb59f3f
Slava Knyazev <slava@knyz.org>
2016-07-09 16:28:06 -0400

    Added multi-line support

--- ressources/openthread.js
+++ ressources/openthread.js
@@ -1,3 +1,8 @@
+String.prototype.replaceAll = function(search, replacement) {
+    var target = this;
+    return target.replace(new RegExp(search, 'g'), replacement);
+};
+
 function getThread(threadid) {
     $.getJSON("threads/" + threadid + ".json", function(json) {
         var data = json;
@@ -9,7 +14,7 @@ function getThread(threadid) {
 }
 
 function openThread(id, title, poster, body, image, time, replies) {
-    var threadOP = "<div class='small-12 columns medium-4 toggleImage'><img src='" + image + "'></div><div class=\"medium-8 small-12 columns preview-text\">\r\n                    <div class=\"row heading\">\r\n                        <h3 class=\"title\">" + title + "<\/h3>\r\n                        <h6>ID: <span class=\"author\">" + poster + "<\/span><\/h6>\r\n                    <\/div>\r\n                    <div class=\"row body\">\r\n                        <p>" + body + "<\/p>\r\n                    <\/div>\r\n                <\/div>";
+    var threadOP = "<div class='small-12 columns medium-4 toggleImage'><img src='" + image + "'></div><div class=\"medium-8 small-12 columns preview-text\"><div class=\"row heading\"><h3 class=\"title\">" + title + "<\/h3><h6>ID: <span class=\"author\">" + poster + "<\/span><\/h6>\r\n                    <\/div><div class=\"row body\"><p>" + body.replaceAll("\n", "<br>") + "<\/p><\/div><\/div>";
 
     $("#thread-dynamic .thread-OP").html(threadOP);
 
@@ -25,7 +30,7 @@ function openThread(id, title, poster, body, image, time, replies) {
 }
 
 function addReply(id, title, poster, body, image, time) {
-    var replyhtml = "<div class=\"small-12 columns preview-text\"><div class=\"row heading\"><h3 class=\"title\">" + title + "<\/h3> <h6>ID: <span class=\"author\">" + poster + "<\/span><\/h6><\/div><div class=\"row body\"><p>" + body + "<\/p><\/div><\/div><\/div><hr>";
+    var replyhtml = "<div class=\"small-12 columns preview-text\"><div class=\"row heading\"><h3 class=\"title\">" + title + "<\/h3> <h6>ID: <span class=\"author\">" + poster + "<\/span><\/h6><\/div><div class=\"row body\"><p>" + body.replaceAll("\n", "<br>") + "<\/p><\/div><\/div><\/div><hr>";
     //Append only if threadid matches
     if ($("#threadopen").data("threadid") == id) {
         $(replyhtml).appendTo("#thread-dynamic .thread-replies");
