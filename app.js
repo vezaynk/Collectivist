9707eb9813a7bd3a974f3547e4616a319968b12e
Slava Knyazev <slava@knyz.org>
2016-07-09 02:23:18 -0400

    Setting up to deploy on heroku

--- app.js
+++ app.js
@@ -333,6 +333,6 @@ io.on('connection', function(socket) {
     });
 });
 
-http.listen(3000, function() {
-    console.log('listening on *:3000');
+http.listen(process.env.PORT || 3000, function() {
+    console.log('listening on *:' + process.env.PORT || 3000);
 });
